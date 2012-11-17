d41d67f65eeff998d11556d41e85ad8b1b68975e
parida <dp297@bath.ac.uk>
2012-11-17 19:10:18 +0000

    get and add event

--- centrePoint/app/controllers/Application.java
+++ centrePoint/app/controllers/Application.java
@@ -15,7 +15,7 @@ public class Application extends Controller {
     
     public static void addEvent()
     {
-    	//new Event(inputTitle, inputCategory, inputDescription, inputStartTime).save();
+    	new Event(inputTitle, inputCategory, inputDescription, inputStartTime).save();
     	render();    
     }
 
@@ -26,14 +26,14 @@ public class Application extends Controller {
     	String inputDescription,
     	String inputStartTime)
     {
-    	//new Event(inputTitle, inputCategory, inputDescription, inputStartTime).save();	
+    	new Event(inputTitle, inputCategory, inputDescription, inputStartTime).save();	
     	render("@Application.index");
     }
     
     public static void getEvent(String name)
     {
-        //Event e = Event.find("byName", name).fetch();
-        //render(e.name);
+        Event e = Event.find("byName", name).fetch();
+        render(e.name);
         
     }
 }
