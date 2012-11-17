2df63249697e13200960b087135d75a8c43fb554
Grant Heywood <grantheywood@gmail.com>
2012-11-17 22:58:42 +0000

    Dale 4

--- centrePoint/app/controllers/Application.java
+++ centrePoint/app/controllers/Application.java
@@ -26,6 +26,12 @@ public class Application extends Controller {
 		render();
 	}
 	
+	public static void venueForm()
+	{
+		render();
+	}
+	
+	
 	public static void addVenue(
     	String inputName,
     	String inputLocation)
